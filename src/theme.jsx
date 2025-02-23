0f866712ede76933131f02dcae21cc947fb240c2
SoloBaChi <solobachi02@gmail.com>
2025-02-23 17:48:55 +0100

    Added darkmode feature

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,6 +1,18 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: props.colorMode === "light" ? "gray.50" : "gray.800",
+        color: props.colorMode === "light" ? "gray.800" : "gray.50",
+      },
+    }),
+  },
   colors: {
     brand: {
       100: '#f7c8c8',
