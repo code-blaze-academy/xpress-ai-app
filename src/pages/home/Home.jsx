9cb68f0efff1dcf7527461b5a608d13138b7aba5
SoloBaChi <solobachi02@gmail.com>
2025-04-05 10:45:29 +0100

    modified some props

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,25 @@ import { Box, Text } from "@chakra-ui/react"
 import CustomButton from "../../components/CustomButton"
 import { FaArrowAltCircleDown } from "react-icons/fa"
 import { FaRProject } from "react-icons/fa6"
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+    const navigate = useNavigate();
+
+     // Function to navigate to the registration page
+     const handleTryForFree = () => {
+      navigate("/register");
+     };
+
   return (
     <Box>
-       <Text textAlign={"center"}>Welcome to home page</Text> 
+       <Text textAlign={"center"} mb={"2rem"}>Welcome to home page</Text> 
        <CustomButton 
-         title="Book Now"
+         title="Try for free"
+         onClick={handleTryForFree}
+         borderRadius={"full"}
+         display={"flex"}
+         mx={"auto"}
        />
     </Box>
   )
